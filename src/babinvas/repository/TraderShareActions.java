ccafb0cbd3de99947daf90b2670ec1de53d79a9a
babinvas <babinvas@gmail.com>
2021-02-14 17:59:31 +0300

    A class for working with the table of traders' operations in the DB has been implemented

--- src/babinvas/repository/TraderShareActions.java
+++ src/babinvas/repository/TraderShareActions.java
@@ -1,4 +1,32 @@
 package babinvas.repository;
 
-public class TraderShareActions {
+import java.sql.SQLException;
+
+public class TraderShareActions extends BaseTable implements TableOperations {
+	public TraderShareActions() throws SQLException {
+		super("trader_share_actions");
+	}
+
+	@Override
+	public void createTable() throws SQLException {
+		super.executeSqlStatement("CREATE TABLE IF NOT EXISTS trader_share_actions(" +
+						"id BIGINT AUTO_INCREMENT PRIMARY KEY," +
+						"operation INTEGER NOT NULL," +
+						"trader INTEGER NOT NULL," +
+						"share_rate INTEGER NOT NULL," +
+						"amount BIGINT NOT NULL",
+				"Создана таблица " + tableName);
+	}
+
+	@Override
+	public void createForeignKeys() throws SQLException {
+		super.executeSqlStatement("ALTER TABLE trader_share_actions " +
+						"ADD FOREIGN KEY (share_rate) " +
+						"REFERENCES share_rates(id)",
+				"Создан внешний ключ trader_share_actions.share -> shares.id");
+	}
+
+	@Override
+	public void createExtraConstraints() throws SQLException {
+	}
 }
