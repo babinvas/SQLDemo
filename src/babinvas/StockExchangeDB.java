1ca0bb81579929aff988f9b3c82dec5c7396edeb
babinvas <babinvas@gmail.com>
2021-02-14 19:31:51 +0300

    The main class was implemented

--- src/babinvas/StockExchangeDB.java
+++ src/babinvas/StockExchangeDB.java
@@ -25,8 +25,8 @@ public class StockExchangeDB {
     Shares shares;
     TraderShareActions traderShareActions;
 
-    //
-    // (Получет новое соединение с БД)
+    // Connects to the database
+    // (Соединяет с БД)
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
     }
@@ -44,6 +44,8 @@ public class StockExchangeDB {
             DeleteDbFiles.execute(DB_DIR, DB_FILE, false);
         }
 
+        // Checks if a JDBC driver is available to work with the database
+        // (Проверяем наличие JDBC драйвера для работы с БД)
         Class.forName(DB_DRIVER);
 
         //
@@ -55,32 +57,31 @@ public class StockExchangeDB {
     }
 
     //
-    // (Создание всех таблиц и внешних ключей)
+    // (Создаёт все таблиц и внешние ключи)
     public void createTablesAndForeignKeys() throws SQLException {
+        shares.createTable();
+        shareRates.createTable();
+        traders.createTable();
+        traderShareActions.createTable();
+
+        //
+        // (Созданаёт ограничения на поля таблиц)
+        traderShareActions.createExtraConstraints();
+        shares.createExtraConstraints();
+
+        //
+        // (Создаёт внешние ключи (связи между таблицами)
+        shareRates.createForeignKeys();
+        traderShareActions.createForeignKeys();
     }
 
     public static void main(String[] args) {
         try {
-            // Checks if a JDBC driver is available to work with the database
-            // (Проверяем наличие JDBC драйвера для работы с БД)
-            Class.forName(DB_DRIVER);
-
-            // Connecting to the database
-            // (Соединение с БД)
-            Connection connection = DriverManager.getConnection(DB_URL);
-            System.out.println("Connection to the DBMS is made.\n" +
-                    "(Соединение с СУБД выполнено).");
-
-            // Disconnecting from the database
-            // (Отключение от БД)
-            connection.close();
-            System.out.println("Disconnection from the DBMS is done.\n" +
-                    "(Отключение от СУБД выполнено).");
-
-
+            StockExchangeDB stockExchangeDB = new StockExchangeDB(true);
+            stockExchangeDB.createTablesAndForeignKeys();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            System.out.println("JDBC драйвер для СУБД не найден");
+            System.out.println("JDBC драйвер для СУБД не найден!");
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Ошибка SQL!");
